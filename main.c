e1b65559bdc2abe745743ae1e28927344b6ad913
sf id : 261623 <71557672+saipoor@users.noreply.github.com>
2021-04-30 18:54:30 +0530

    Create main.c

---
+++ main.c
@@ -0,0 +1,29 @@
+#include "activity4.h"
+#include "activity3.h"
+#include "activity2.h"
+#include "activity1.h"
+#include <util/delay.h>
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        port_initialize(); 
+        int check_status_led=0;
+        check_status_led=led_init(); 
+        if(check_status_led==1)
+        {
+            Timer_Initialize();
+            ADC_Initialize();
+            uint16_t temp = 0;
+            USART_initialize(103);
+			temp = ReadADC(0);
+			_delay_ms(200);
+			pulse_wave(temp);	
+			_delay_ms(200);
+			USARTWritemsg(temp);
+        }
+    }
+    return 0;
+}
