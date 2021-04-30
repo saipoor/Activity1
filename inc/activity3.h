6facba2aaafbc8cc8c6cd702f3a6d2585117ccbe
sf id : 261623 <71557672+saipoor@users.noreply.github.com>
2021-04-30 18:49:14 +0530

    Create activity3.h

---
+++ inc/activity3.h
@@ -0,0 +1,14 @@
+#ifndef ACTIVITY3_H_INCLUDED
+#define ACTIVITY3_H_INCLUDED
+
+/**
+ * @brief Timer intialisation
+ * 
+ */
+void Timer_Initialize(void);
+/**
+ * @brief Generation of pulsewm
+ * 
+ */
+void pulse_wave(uint16_t);
+#endif 
