efdac2b199271e715a150352eb87086f02cf7cc9
sf id : 261623 <71557672+saipoor@users.noreply.github.com>
2021-04-30 18:18:58 +0530

    Create activity1.c

---
+++ src/activity1.c
@@ -0,0 +1,29 @@
+#include "activity1.h"
+#include <util/delay.h>
+#include <avr/io.h>
+
+void port_initialize()
+{
+    DDRB|=(1<<PB0); 
+    DDRD&=~(1<<PD0);
+    DDRD&=~(1<<PD4);
+    PORTD|=(1<<PD0);
+    PORTD|=(1<<PD4);
+    return 0;
+}
+
+int led_initialize()
+{
+        if((!(PIND & (1<<PD0)) && (!(PIND & (1<<PD4)))))
+        {
+            PORTB |=(1<<PB0);//Instruction used for led on
+            _delay_ms(2000);
+            return 1;
+        }
+        else
+        {
+            PORTB &=~(1<<PB0);//Instruction used for led off
+            _delay_ms(2000);
+            return 0;
+        }
+}
